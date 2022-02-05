d030acfe957bdb6774be5f0eedd87bbe0368ead9
dare <gdare1999@gmail.com>
2022-02-05 20:59:06 +0900

    implement UI of register page

--- app/front-end/README.md
+++ app/front-end/README.md
@@ -3,6 +3,7 @@
 ## Project setup
 ```
 npm install
+npm install jquery popper.js bootstrap --save 
 ```
 
 ### Compiles and hot-reloads for development
--- app/front-end/vue.config.js
+++ app/front-end/vue.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   devServer: {
-    port:3000,
+    clientLogLevel: 'info',
+    port: 3000,
     proxy: {
       '/api/*': {
         target: 'http://localhost:8080'
